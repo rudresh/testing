9e160575919f0c5e86015ad6641489b241254402
Vaibhav Rathore <vaibhav@coloredcow.in>
2017-12-12 16:38:30 +0530

    new test case

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Soiree
+                    Soiree Test
                 </div>
             </div>
         </div>
